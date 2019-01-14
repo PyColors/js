db89491793d16e2e84e984dbd0b077410b825ace
PyColors <patweb44@gmail.com>
2019-01-14 20:45:59 +0000

    Adding Promesses

--- test.js
+++ test.js
@@ -3,3 +3,25 @@ import {add, sub} from './modules'
 console.log(add(1, 2))
 console.log(sub(1, 2))
 
+
+
+const func = () => console.log('coco')
+setTimeout(func, 3000)
+
+
+
+const promise = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('coco')
+            reject(40)
+        }, 2000)
+    })
+
+promise()
+    .then(elem => {
+        console.log('toto', elem)
+    })
+    .catch(err => {
+        throw new Error(err)
+    })
